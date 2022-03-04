7108f2f5dc415167c92dfe6cd1b7e90ca90af2bd
haianh123 <lehaianh111103@gmail.com>
2022-03-04 16:28:03 +0700

    123

---
+++ index.js
@@ -0,0 +1,22 @@
+function sum(a,b){
+    const tong = a+b
+    console.log(`tong:${tong}`)
+}
+function tru(a,b){
+    const tru = a+b
+    console.log(`tru:${tong}`)
+}
+function nhan(a,b){
+    const nhan = a+b
+    console.log(`nhan:${tong}`)
+}
+function chia(a,b){
+    const chia = a+b
+    console.log(`chia:${tong}`)
+}
+module.exports = {
+    sum,
+    tru,
+    nhan,
+    chia
+}
